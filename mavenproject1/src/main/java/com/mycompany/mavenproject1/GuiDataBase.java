fd574b3effc89739032139ea65fc9042f8d8a7ba
gomaa92 <31782459+gomaa92@users.noreply.github.com>
2019-04-06 17:51:28 +0200

    branch featured commit1

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/GuiDataBase.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/GuiDataBase.java
@@ -6,8 +6,13 @@
 package com.mycompany.mavenproject1;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -28,15 +33,121 @@ public class GuiDataBase extends Application {
         resultSet = root.dataBaseConnection();
     }
     
-    // islam start here
+      
+
+    
+    
      @Override
     public void start(Stage primaryStage) {
             Scene scene = new Scene(root);
+            
+        root.newRow.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent value) {
+                if (!status) {
 
+                    root.id.setText("");
+                    root.fName.setText("");
+                    root.mName.setText("");
+                    root.lName.setText("");
+                    root.eMail.setText("");
+                    root.phone.setText("");
+
+                    status = true;
+
+                }
+            }
+        });
+        
+        root.update.setOnAction((event) -> {
+            try {
+                if (!status) {
+
+                    updateRow();
+                    resultSet.updateRow();
+                } else {
+                    resultSet.moveToInsertRow();
+
+                    updateRow();
+                    resultSet.insertRow();
+                    status = false;
+
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        });
+        
+        root.delete.setOnAction(value -> {
+            try {
+                if (!status) {
+                    resultSet.deleteRow();
+                    resultSet.first();
+                    showResult();
+                } else {
+                    System.out.println("please select row");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        });
+         root.last.setOnAction(value -> {
+
+            try {
+                if (resultSet.last()) {
+                    root.id.setText(resultSet.getInt(1) + "");
+                    root.fName.setText(resultSet.getString(2));
+                    root.mName.setText(resultSet.getString(3));
+                    root.lName.setText(resultSet.getString(4));
+                    root.eMail.setText(resultSet.getString(5));
+                    root.phone.setText(resultSet.getInt(6) + "");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+
+
+        
+    //islam start here 
         primaryStage.setTitle("DAataBase");
         primaryStage.setScene(scene);
         primaryStage.show();
 }
+    
+        private void showResult() {
+        try {
+            root.id.setText(resultSet.getInt(1) + "");
+            root.fName.setText(resultSet.getString(2));
+            root.mName.setText(resultSet.getString(3));
+            root.lName.setText(resultSet.getString(4));
+            root.eMail.setText(resultSet.getString(5));
+            root.phone.setText(resultSet.getInt(6) + "");
+        } catch (SQLException ex) {
+            Logger.getLogger(GuiDataBase.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+        
+    
+    private void updateRow() {
+        try {
+            resultSet.updateInt(1, Integer.parseInt(root.id.getText()));
+            resultSet.updateString(2, root.fName.getText());
+            resultSet.updateString(3, root.mName.getText());
+            resultSet.updateString(4, root.lName.getText());
+            resultSet.updateString(5, root.eMail.getText());
+            resultSet.updateInt(6, Integer.parseInt(root.phone.getText()));
+        } catch (SQLException ex) {
+            Logger.getLogger(GuiDataBase.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
     
      public static void main(String[] args) {
         launch(args);
