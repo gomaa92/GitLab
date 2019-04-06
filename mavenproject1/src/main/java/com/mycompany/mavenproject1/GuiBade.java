ca24d9a02bc088b6a8a290590f7d07365664554f
gomaa92 <31782459+gomaa92@users.noreply.github.com>
2019-04-06 16:19:56 +0200

    data Base Connection

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/GuiBade.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author Gom3a
+ */
+public class GuiBade {
+    
+    ResultSet resultSet = null;
+    Statement statement = null;
+   
+        ResultSet dataBaseConnection() {
+        DataSource dataSource = null;
+        dataSource = MyDataSourceFactory.getMySQLDataSource();
+        Connection connection = null;
+
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+
+            try {
+                String queryString = "select * from user";
+                resultSet = statement.executeQuery(queryString);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return resultSet;
+    }
+    
+}
